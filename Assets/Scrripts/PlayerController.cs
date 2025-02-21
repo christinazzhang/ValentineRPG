c9b7cafd13d483bee3808d696a75f055e8ecec49
christinazzhang <christina.916.zhang@gmail.com>
2025-02-21 02:06:45 -0500

    collisions

---
+++ Assets/Scrripts/PlayerController.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    private bool isMoving;
+    public Vector2 input;
+    private Animator animator;
+    private LayerMask solidObjectslayer;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isMoving)
+        {
+            input.x = Input.GetAxisRaw("Horizontal");
+            input.y = Input.GetAxisRaw("Vertical");
+
+
+            if (input.x != 0) input.y = 0;
+
+            if (input != Vector2.zero)
+            {
+                animator.SetFloat("moveX", input.x);
+                animator.SetFloat("moveY", input.y);
+                var targetPos = transform.position;
+                targetPos.x += input.x;
+                targetPos.y += input.y;
+
+                if(isWalkable(targetPos))
+                {
+                    StartCoroutine(Move(targetPos));
+                }
+            }
+        }
+
+        animator.SetBool("isMoving", isMoving);
+    }
+
+    IEnumerator Move(Vector3 targetPos)
+    {
+        isMoving = true;
+        while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+
+        transform.position = targetPos;
+        isMoving = false;
+    }
+
+    private bool isWalkable(Vector3 targetPos) { 
+        if (Physics2D.OverlapCircle(targetPos, 0.2f, solidObjectslayer) != null)
+        {
+            return false;
+        }
+        return true;
+    }
+}
